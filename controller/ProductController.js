e5de2a3caada08d7ac61149d4b14aae4c4924850
Shwetha <shwetharamesh80@gmail.com>
2025-02-03 10:12:27 +0530

    first commit

---
+++ controller/ProductController.js
@@ -0,0 +1,44 @@
+const Product = require('../model/Product');
+
+
+exports.getAllProducts = async (req, res) =>{
+    try{
+        const products = await Product.find()
+        res.json(products)
+    }
+    catch(err){
+        res.status(500).json({msg : err.message})
+    }
+}
+
+exports.getSingleProduct = async (req, res) =>{
+    try {
+        const product = await Product.findOne({proId: req.params.id})
+        if(!product)
+            return res.status(404).json({msg: 'Product Not found'})
+        
+        res.json(product)
+    }
+    catch(err){
+        res.status(500).json({msg : err.message})
+    }
+}
+
+exports.addNewProduct = async (req, res)=>{
+    try{
+        const newProduct = req.body;
+        const check = await Product.findOne({proId:newProduct.proId})
+        if(!check){            
+            await Product.create(req.body);
+            res.json(newProduct)
+        }
+        else
+            return res.status(500).json({msg:'Record already exixts'})     
+    }
+    catch(err){
+        res.status(500).json({msg:err.message})
+    }
+
+}
+
+
